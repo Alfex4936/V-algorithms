04aa8a8397a50b33e37972e6d4a8cfdb351bf6fa
Seok Won <alfex4936@gmail.com>
2020-08-18 21:51:14 +0900

    V : Bubble Sort
    with random test array
    
    length of 5,000 took ~0.26s
    
    while python took ~2.5s

---
+++ sorting/bubble_sort.v
@@ -0,0 +1,33 @@
+import time
+import rand
+
+const (
+	gen_len = 10_000 // how many random numbers to generate
+	gen_max = 10_000 // max of the generated numbers
+)
+
+fn main() {
+	mut test_arr := []int{}
+	for _ in 0..gen_len {
+		test_arr << rand.int_in_range(-gen_max, gen_max)
+	}
+	println('Random array length : $test_arr.len')
+	sw := time.new_stopwatch({})
+	bubble_sort<int>(mut test_arr)
+	println('Took : ${sw.elapsed().milliseconds()}ms')
+	println('Result : $test_arr')
+}
+
+
+// Bubble Sort
+fn bubble_sort<T>(mut array []int) {
+	for i in 0..array.len {
+		for j in 0..array.len - 1 - i {
+			if array[j] > array[j + 1] {
+				array[j], array[j + 1] = array[j + 1], array[j]
+			}
+		}
+	}
+	// println('${array[i]}')
+}
+
