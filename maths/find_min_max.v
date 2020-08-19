c8b24840957c7f7fc94d52b4086c6e22581022bc
Seok Won <alfex4936@gmail.com>
2020-08-19 18:23:04 +0900

    V : Find min and max in array

---
+++ maths/find_min_max.v
@@ -0,0 +1,22 @@
+fn main() {
+    test_arr := [1, 0, -3, 5, 99, -4]
+
+    min, max := find_min_max(test_arr)
+    println('$min and $max')  // -4 and 99
+}
+
+fn find_min_max(array []int) (int, int) {
+    mut min := array[0]
+    mut max := array[0]
+
+    for i in 0..array.len {
+        if array[i] > max {
+            max = array[i]
+        }
+
+        if array[i] < min {
+            min = array[i]
+        }
+    }
+    return min, max
+}
