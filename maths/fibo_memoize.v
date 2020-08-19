effcb2fdb4be973e1d76b9981c2221bba7c5b58a
Seok Won <alfex4936@gmail.com>
2020-08-19 16:17:41 +0900

    V : Fibonacci Number
    1st = 0, 2nd = 1, 3rd = 1, 4th = 2 ...
    
    using memoization
    
    currently, V lang does not support map[int]int type
    
    so I used map[string][int] and conversion()

---
+++ maths/fibo_memoize.v
@@ -0,0 +1,20 @@
+fn main() {
+    // map[int]int not supported yet
+    mut memoize := {
+        '1' : 0
+        '2' : 1
+    }
+
+   println(get_n_fibo(18, mut memoize))
+   println(memoize)
+
+}
+
+fn get_n_fibo(n int, mut memo map[string]int) int {
+    if n.str() in memo {
+        return memo[n.str()]
+    } else {
+        memo[n.str()] = get_n_fibo(n - 1, mut memo) + get_n_fibo(n - 2, mut memo)
+        return memo[n.str()]
+    }
+}
